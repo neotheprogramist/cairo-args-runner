37c752920e391babd68d7ec596ab43030ecbf72f
Neo <neotheprogramist@proton.me>
2023-12-21 16:57:58 +0100

    clap integration

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,15 +5,47 @@ use cairo_args_runner::utils::logger::{LoggerCompiler, LoggerGenerator, LoggerPa
 use cairo_args_runner::utils::parse::SierraParser;
 use cairo_args_runner::utils::run::SierraRunner;
 use cairo_felt::Felt252;
-use cairo_lang_runner::Arg;
+use cairo_lang_runner::Arg as CairoRunnerArg;
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to the folder of the Scarb project
+    #[arg(long)]
+    folder: String,
+
+    /// Package name of the Scarb project
+    #[arg(long)]
+    package: String,
+
+    /// Function to run
+    #[arg(long)]
+    function: String,
+
+    /// Numeric arguments to pass to the function
+    #[arg(long)]
+    values: Vec<u128>,
+}
 
 fn main() -> Result<()> {
-    let generator = LoggerGenerator::new(Generator::new("examples/fib", "fib"));
+    let args = Args::parse();
+
+    let generator = LoggerGenerator::new(Generator::new(args.folder, args.package));
     let compiler = LoggerCompiler::new(generator.generate()?);
     let parser = LoggerParser::new(compiler.compile()?);
     let runner = parser.parse()?;
-    let arguments = vec![Arg::Value(Felt252::new(10))];
-    let result = runner.run("::main", &arguments).unwrap();
+
+    let arguments = args
+        .values
+        .into_iter()
+        .map(|num| CairoRunnerArg::Value(Felt252::new(num)))
+        .collect::<Vec<_>>();
+
+    let result = runner
+        .run(format!("::{}", args.function).as_str(), &arguments)
+        .unwrap();
     println!("Result: {:?}", result);
     Ok(())
 }

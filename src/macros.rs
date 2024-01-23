710d71c5438484098124f129bfb3413c27ca9430
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2024-01-23 11:07:25 +0000

    macros use public exports

--- src/macros.rs
+++ src/macros.rs
@@ -8,14 +8,14 @@ macro_rules! felt_vec {
 #[macro_export]
 macro_rules! arg_value {
     ($a:expr) => {
-        cairo_args_runner::Arg::Value(cairo_felt::Felt252::from($a))
+        cairo_args_runner::Arg::Value(cairo_args_runner::Felt252::from($a))
     };
 }
 
 #[macro_export]
 macro_rules! arg_value_vec {
     ($($a:expr),*) => {
-        vec![$(cairo_args_runner::Arg::Value(cairo_felt::Felt252::from($a))),*]
+        vec![$(cairo_args_runner::Arg::Value(cairo_args_runner::Felt252::from($a))),*]
     };
 }
 
